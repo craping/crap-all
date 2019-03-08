fa2aa880119133242a1d25e62709ccd79acb7eb0
Crap <422655094@qq.com>
2019-03-08 17:28:16 +0800

    修改Disruptor多消费者

---
+++ org.crap.jrain/org.crap.jrain.mvc/src/main/java/org/crap/jrain/mvc/netty/disruptor/Executor.java
@@ -0,0 +1,25 @@
+package org.crap.jrain.mvc.netty.disruptor;
+
+import com.lmax.disruptor.WorkHandler;
+
+import io.netty.channel.Channel;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.util.ReferenceCountUtil;
+
+public class Executor implements WorkHandler<RequestEvent<FullHttpRequest, Channel>> {
+
+	@Override
+	public void onEvent(RequestEvent<FullHttpRequest, Channel> event) throws Exception {
+		try {
+    		event.getTreatment().process(event.getMapping(), event.getRequest(), event.getResponse());
+        } catch (Exception e) {
+        	e.printStackTrace();
+        	event.getResponse().close();
+		} finally {
+			if(event.getRequest().content().isReadable())
+				ReferenceCountUtil.release(event.getRequest());
+            event.clear();
+        }
+	}
+
+}
